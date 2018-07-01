44c7dbf1bd309fae2c11775eadd15991a50a2e1c
Ronak SHAH <ronak1009@gmail.com>
2018-07-01 23:12:32 +0530

    practice exercise for day 04

--- basic-cplusplus/day03/practice/Source04.cpp
+++ basic-cplusplus/day03/practice/Source04.cpp
@@ -1,1 +1,41 @@
 //Write a program that demonstrates probability search.
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define N 5
+int probSearch(int inputArr[], int size, int searchNo);
+
+int main() {
+	srand(time_t(NULL));
+	int a[N] = { 0 };
+	for (int i = 0; i < N; i++)
+		a[i] = rand() % 100;
+
+	int searchQuery = 12;
+	int search = probSearch(a, N, searchQuery);
+	if (search >= 0)
+		printf("Searched Index:: %d", a[search]);
+	else
+		printf("Item not found");
+
+}
+
+int probSearch(int inputArr[], int size, int searchNo) {
+	int returnIndex = -1;
+	for (int i = 0; i < size; i++) {
+		if (searchNo == inputArr[i] && i > 0) {
+			//swapping the queried item of the array
+			int temp = inputArr[i - 1];
+			inputArr[i - 1] = inputArr[i];
+			inputArr[i] = temp;
+			returnIndex = i-1;
+			break;
+		}
+		else if (searchNo == inputArr[i]) {
+			returnIndex = i;
+			break;
+		}
+	}
+	return returnIndex;
+}
--- basic-cplusplus/day04/practice/Source01.cpp
+++ basic-cplusplus/day04/practice/Source01.cpp
@@ -2,4 +2,55 @@
 Write a random function such that it produces random int on every call and returns same as its return value.
 Use mid sqaured for the generation of random integers.
 */
+#include <stdio.h>
+#include <string>
 
+#define N 3
+
+int midSquareRandom();
+int obtainMiddle(int input);
+int digitCounter(int number);
+
+int main() {
+	int a[N] = { 0 };
+	for (int i = 0; i < N; i++) {
+		a[i] = midSquareRandom();
+		printf("a[%d] = %d\n", i, a[i]);
+	}
+
+}
+
+int midSquareRandom() {
+	static int previousNo = -1;
+	int seedValue = previousNo > 0 ? previousNo : 1234;
+	int square = seedValue * seedValue;
+
+	//obtain middle 4 digits
+	int random = obtainMiddle(square);
+	if (random == previousNo)
+		return 0;
+	else {
+		previousNo = random;
+		return random;
+	}
+}
+
+
+int obtainMiddle(int input) {
+	//compute the no of digits
+	int noOfDigits = digitCounter(input);
+	int returnValue = input;
+	returnValue /= 100;
+	returnValue %= 10000;
+	return returnValue;
+}
+
+int digitCounter(int number) {
+	int noOfDigits = 0;
+	int fract = number;
+	while (fract > 0) {
+		noOfDigits++;
+		fract /= 10;
+	}
+	return noOfDigits;
+}
